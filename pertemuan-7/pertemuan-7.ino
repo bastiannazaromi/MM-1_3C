904f9d1c3fc6c64e4af6409973da3b672ac3560a
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2025-11-06 14:53:52 +0700

    source code lcd i2c

--- pertemuan-7/pertemuan-7.ino
+++ pertemuan-7/pertemuan-7.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h> // Library komunikasi I2C 
+#include <LiquidCrystal_I2C.h> // Library modul I2C LCD
 
+// default address 0x27 
+// tipe LCD 16x2 (16,2)
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); 
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.print("Modul I2C LCD");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
 }
