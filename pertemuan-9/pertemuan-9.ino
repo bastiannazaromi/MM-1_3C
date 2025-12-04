d648fca0eadc60b129035c3906075bf398455339
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2025-12-04 16:11:36 +0700

    pertemuan 9 done

--- pertemuan-9/pertemuan-9.ino
+++ pertemuan-9/pertemuan-9.ino
@@ -14,10 +14,15 @@ LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x3F, 16, 2);
 
 DHT dht(DHTPIN, DHTTYPE);
 
+#define relayPin D2
+
 void setup() {
   Serial.begin(9600);
   
   dht.begin();
+
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
 
   lcd.init();
   lcd.backlight();
@@ -32,10 +37,10 @@ void setup() {
 }
 
 void loop() {
-  float suhu = dht.readTemperature();
-  float kelembapan = dht.readHumidity();
+  int suhu = dht.readTemperature();
+  int kelembapan = dht.readHumidity();
   // suhu dalam fahrenheit
-//  float f = dht.readTemperature(true);
+  // float f = dht.readTemperature(true);
   
   if (isnan(suhu) || isnan(kelembapan)) {
     Serial.println("Failed to read from DHT sensor!");
@@ -56,8 +61,18 @@ void loop() {
     lcd.print("KELEMBAPAN : ");
     lcd.setCursor(13, 1);
     lcd.print(kelembapan);
-  }
 
+    if (suhu <= 27) {
+      digitalWrite(relayPin, HIGH);
+
+      Serial.println("Kipas ON");
+    } else {
+      digitalWrite(relayPin, LOW);
+
+      Serial.println("Kipas OFF");
+    }
+  }
+  
   Serial.println();
   delay(1000);
 }
