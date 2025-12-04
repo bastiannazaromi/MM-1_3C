67e7dcb482a79e95df340a103c4c2a9522804743
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2025-12-04 14:30:44 +0700

    pertemuan 9

---
+++ pertemuan-9/pertemuan-9.ino
@@ -0,0 +1,62 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
+
+// SCL --> D1
+// SDA --> D2
+
+#include "DHT.h"
+
+#define DHTPIN D2
+
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  
+  dht.begin();
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+  lcd.setCursor(4, 0);
+  lcd.print("KELAS 3C");
+
+  Serial.println("Program Sensor DHT");
+
+  delay(1000);
+}
+
+void loop() {
+  float suhu = dht.readTemperature();
+  float kelembapan = dht.readHumidity();
+  // suhu dalam fahrenheit
+  float f = dht.readTemperature(true);
+  
+  if (isnan(suhu) || isnan(kelembapan)) {
+    Serial.println("Failed to read from DHT sensor!");
+    
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("FAILED READ DHT");
+  } else {
+    Serial.println("SUHU : " + (String) suhu);
+    Serial.println("Kelembapan : " + (String) kelembapan);
+    
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("SUHU : ");
+    lcd.setCursor(7, 0);
+    lcd.print(suhu);
+    lcd.setCursor(0, 1);
+    lcd.print("KELEMBAPAN : ");
+    lcd.setCursor(13, 0);
+    lcd.print(kelembapan);
+  }
+
+  delay(1000);
+}
