0ce3b3fc582a9490d12fefe575026514c9d09bd9
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2026-01-07 08:27:40 +0700

    minggu ke 11

--- pertemuan-11/pertemuan-11.ino
+++ pertemuan-11/pertemuan-11.ino
@@ -1,23 +1,47 @@
 #define pinLdr A0
 #define pinSensorHujan A1
 
+#include <Servo.h>
+
+Servo motorServo;
+#define pinMotorServo 3
+
+#define pinLed 4
+
 void setup()
 {
   Serial.begin(9600);
 
+  motorServo.attach(pinMotorServo);
+  motorServo.write(0);
+
+  pinMode(pinLed, OUTPUT);
   Serial.println("KELAS 3C");
 }
 
 void loop()
 {
   int nilaiLdr = analogRead(pinLdr);
-  // nilaiLdr = map(value, 0, 1023, 0, 100);
+  nilaiLdr = map(nilaiLdr, 0, 1023, 100, 0);
 
   int nilaiSensorHujan = analogRead(pinSensorHujan);
-  // nilaiSensorHujan = map(value, 0, 1023, 0, 100);
+  nilaiSensorHujan = map(nilaiSensorHujan, 0, 1023, 100, 0);
   
   Serial.println("Nilai LDR : " + (String) nilaiLdr);
   Serial.println("Nilai Sensor Hujan : " + (String) nilaiSensorHujan);
   
-  delay(200);
+  if (nilaiLdr < 25) {
+    // .....
+  } else {
+    // .....
+  }
+
+  if (nilaiSensorHujan > 50) {
+    // ....
+  } else {
+    // ....
+  }
+  
+  Serial.println();
+  delay(1000);
 }
