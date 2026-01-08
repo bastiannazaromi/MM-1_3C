48cbb418d0f72930b80f7b79065f9f08f3a690ad
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2026-01-08 14:09:55 +0700

    pertemuan 12

---
+++ pertemuan-12/basic/basic.ino
@@ -0,0 +1,50 @@
+#define IR_MASUK 9
+#define IR_KELUAR 10
+
+#define LED_MASUK  2
+#define LED_KELUAR 3
+
+unsigned long lastCheckMasuk = 0;
+unsigned long lastCheckKeluar = 0;
+const unsigned long interval = 100;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(IR_MASUK, INPUT);
+  pinMode(IR_KELUAR, INPUT);
+
+  pinMode(LED_MASUK, OUTPUT);
+  pinMode(LED_KELUAR, OUTPUT);
+
+  digitalWrite(LED_MASUK, LOW);
+  digitalWrite(LED_KELUAR, LOW);
+
+  Serial.println("SISTEM COUNTER AKTIF");
+}
+
+void loop() {
+  unsigned long now = millis();
+
+  if (now - lastCheckMasuk >= interval) {
+    lastCheckMasuk = now;
+
+    if (digitalRead(IR_MASUK) == HIGH) {
+      digitalWrite(LED_MASUK, HIGH);
+      Serial.println("Orang MASUK");
+    } else {
+      digitalWrite(LED_MASUK, LOW);
+    }
+  }
+
+  if (now - lastCheckKeluar >= interval) {
+    lastCheckKeluar = now;
+
+    if (digitalRead(IR_KELUAR) == HIGH) {
+      digitalWrite(LED_KELUAR, HIGH);
+      Serial.println("Orang KELUAR");
+    } else {
+      digitalWrite(LED_KELUAR, LOW);
+    }
+  }
+}
---
+++ pertemuan-12/pertemuan-12.ino
@@ -0,0 +1,60 @@
+#define IR_MASUK 9
+#define IR_KELUAR 10
+
+#define LED_MASUK 2
+#define LED_KELUAR 3
+
+unsigned long lastCheckMasuk = 0;
+unsigned long lastCheckKeluar = 0;
+const unsigned long interval = 50;
+
+int jumlahOrang = 0;
+
+int lastMasukState = LOW;
+int lastKeluarState = LOW;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(IR_MASUK, INPUT);
+  pinMode(IR_KELUAR, INPUT);
+
+  pinMode(LED_MASUK, OUTPUT);
+  pinMode(LED_KELUAR, OUTPUT);
+
+  Serial.println("KELAS 3C");
+}
+
+void loop() {
+  unsigned long now = millis();
+
+  if (now - lastCheckMasuk >= interval) {
+    lastCheckMasuk = now;
+
+    int currentMasuk = digitalRead(IR_MASUK);
+
+    if (currentMasuk == HIGH && lastMasukState == LOW) {
+      jumlahOrang++;
+      Serial.print("MASUK | Jumlah orang: ");
+      Serial.println(jumlahOrang);
+    }
+
+    digitalWrite(LED_MASUK, currentMasuk);
+    lastMasukState = currentMasuk;
+  }
+
+  if (now - lastCheckKeluar >= interval) {
+    lastCheckKeluar = now;
+
+    int currentKeluar = digitalRead(IR_KELUAR);
+
+    if (currentKeluar == HIGH && lastKeluarState == LOW) {
+      if (jumlahOrang > 0) jumlahOrang--;
+      Serial.print("KELUAR | Jumlah orang: ");
+      Serial.println(jumlahOrang);
+    }
+
+    digitalWrite(LED_KELUAR, currentKeluar);
+    lastKeluarState = currentKeluar;
+  }
+}
