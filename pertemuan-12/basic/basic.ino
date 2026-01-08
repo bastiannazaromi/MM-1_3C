86e0dcfe478d87b094dac9b78e7993991c48a11b
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2026-01-08 14:23:47 +0700

    pertemuan 12

--- pertemuan-12/basic/basic.ino
+++ pertemuan-12/basic/basic.ino
@@ -1,50 +1,26 @@
-#define IR_MASUK 9
-#define IR_KELUAR 10
-
-#define LED_MASUK  2
-#define LED_KELUAR 3
-
-unsigned long lastCheckMasuk = 0;
-unsigned long lastCheckKeluar = 0;
-const unsigned long interval = 100;
+#define IRSensor 9
+#define LED 2
 
 void setup() {
   Serial.begin(9600);
+  
+  pinMode(IRSensor, INPUT);
+  pinMode(LED, OUTPUT);
 
-  pinMode(IR_MASUK, INPUT);
-  pinMode(IR_KELUAR, INPUT);
-
-  pinMode(LED_MASUK, OUTPUT);
-  pinMode(LED_KELUAR, OUTPUT);
-
-  digitalWrite(LED_MASUK, LOW);
-  digitalWrite(LED_KELUAR, LOW);
-
-  Serial.println("SISTEM COUNTER AKTIF");
+  Serial.println("KELAS 3C");
 }
 
 void loop() {
-  unsigned long now = millis();
-
-  if (now - lastCheckMasuk >= interval) {
-    lastCheckMasuk = now;
-
-    if (digitalRead(IR_MASUK) == HIGH) {
-      digitalWrite(LED_MASUK, HIGH);
-      Serial.println("Orang MASUK");
-    } else {
-      digitalWrite(LED_MASUK, LOW);
-    }
+  int sensorStatus = digitalRead(IRSensor);
+  if (sensorStatus == 1)
+  {
+    digitalWrite(LED, LOW);
+    Serial.println("Terdeteksi");
+  }
+  else  {
+    digitalWrite(LED, HIGH);
+    Serial.println("Tidak ada pergerakan");
   }
 
-  if (now - lastCheckKeluar >= interval) {
-    lastCheckKeluar = now;
-
-    if (digitalRead(IR_KELUAR) == HIGH) {
-      digitalWrite(LED_KELUAR, HIGH);
-      Serial.println("Orang KELUAR");
-    } else {
-      digitalWrite(LED_KELUAR, LOW);
-    }
-  }
+  delay(500);
 }
