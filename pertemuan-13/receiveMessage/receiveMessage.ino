6e31478f63816544210a86dada633e35ad12280d
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2026-01-15 13:58:47 +0700

    pertemuan 14

---
+++ pertemuan-13/receiveMessage/receiveMessage.ino
@@ -0,0 +1,74 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "NAMA_WIFI";
+const char* password = "PASSWORD_WIFI";
+
+#define BOT_TOKEN "ISI_BOT_TOKEN"
+#define CHAT_ID   "ISI_CHAT_ID"
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOT_TOKEN, client);
+
+unsigned long lastTimeBotRan;
+const unsigned long botRequestDelay = 2000;
+
+void handleNewMessages(int numNewMessages) {
+  Serial.println("Pesan baru diterima");
+
+  for (int i = 0; i < numNewMessages; i++) {
+    String chat_id = bot.messages[i].chat_id;
+    String text    = bot.messages[i].text;
+    String from    = bot.messages[i].from_name;
+
+    if (from == "") from = "User";
+
+    Serial.println("------------");
+    Serial.print("Dari   : ");
+    Serial.println(from);
+    Serial.print("Chat ID: ");
+    Serial.println(chat_id);
+    Serial.print("Pesan  : ");
+    Serial.println(text);
+
+    if (text == "/start") {
+      bot.sendMessage(chat_id, "ESP8266 siap menerima perintah 🚀", "");
+    }
+
+    if (text == "ping") {
+      bot.sendMessage(chat_id, "pong", "");
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("ESP8266 Telegram Receiver");
+
+  WiFi.begin(ssid, password);
+  Serial.print("Menghubungkan ke WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi terhubung!");
+  Serial.println(WiFi.localIP());
+
+  client.setInsecure();
+}
+
+void loop() {
+  if (millis() > lastTimeBotRan + botRequestDelay) {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    if (numNewMessages) {
+      handleNewMessages(numNewMessages);
+    }
+
+    lastTimeBotRan = millis();
+  }
+}
---
+++ pertemuan-13/sendMessage/sendMessage.ino
@@ -0,0 +1,53 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "NAMA_WIFI";
+const char* password = "PASSWORD_WIFI";
+
+#define BOT_TOKEN "ISI_BOT_TOKEN"
+#define CHAT_ID   "ISI_CHAT_ID"
+j
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOT_TOKEN, client);
+
+String serialMessage = "";
+
+void setup() {
+  Serial.begin(9600);
+  
+  WiFi.begin(ssid, password);
+  Serial.print("Menghubungkan ke WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi terhubung!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  client.setInsecure();
+}
+
+void loop() {
+  while (Serial.available()) {
+    char c = Serial.read();
+
+    if (c == '\n' || c == '\r') {
+      if (serialMessage.length() > 0) {
+        Serial.print("Mengirim ke Telegram: ");
+        Serial.println(serialMessage);
+
+        bot.sendMessage(CHAT_ID, serialMessage, "");
+
+        serialMessage = "";
+      }
+    } 
+    else {
+      serialMessage += c;
+    }
+  }
+}
